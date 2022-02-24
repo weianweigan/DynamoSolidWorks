7d7274903532a9058516d57d9656cfda9c73211a
dududu <1831197727@qq.com>
2022-02-24 18:26:10 +0800

    Add installer project

---
+++ src/Installer/Installer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using WixSharp;
+using WixSharp.CommonTasks;
+using WixSharp.Controls;
+
+const string installationDir = @"%AppDataFolder%\Autodesk\Revit\Addins\";
+const string projectName = "DynamoSldWorks";
+const string outputName = "DynamoSldWorks";
+const string outputDir = "output";
+var version = "1.0.2";
+var fileName = new StringBuilder().Append(outputName).Append("-").Append(version);
+
+var project = new Project
+{
+    Name = projectName,
+    OutDir = outputDir,
+    Platform = Platform.x64,
+    UI = WUI.WixUI_InstallDir,
+    Version = new Version(version),
+    OutFileName = fileName.ToString(),
+    InstallScope = InstallScope.perUser,
+    MajorUpgrade = MajorUpgrade.Default,
+    GUID = new Guid("62E8D571-F797-428D-A8A5-BDEAE1EADDF9"),
+    BackgroundImage = @"Installer\Resources\Icons\logo_about.png",
+    BannerImage = @"Installer\Resources\Icons\logo_about.png",
+    ControlPanelInfo =
+    {
+        Manufacturer = "WeiGan",
+        HelpLink = "https://github.com/weianweigan/DynamoSolidWorks",
+        ProductIcon = @"Installer\Resources\Icons\ShellIcon.ico"
+    },
+    Dirs = new Dir[]
+    {
+        new InstallDir(installationDir, GenerateWixEntities())
+    }
+};
+
+MajorUpgrade.Default.AllowSameVersionUpgrades = true;
+project.RemoveDialogsBetween(NativeDialogs.WelcomeDlg, NativeDialogs.InstallDirDlg);
+project.BuildMsi();
+
+WixEntity[] GenerateWixEntities()
+{
+    var versionRegex = new Regex(@"\d+");
+    var versionStorages = new Dictionary<string, List<WixEntity>>();
+
+    foreach (var directory in args)
+    {
+        var directoryInfo = new DirectoryInfo(directory);
+        var fileVersion = versionRegex.Match(directoryInfo.Name).Value;
+        var files = new Files($@"{directory}\*.*");
+        if (versionStorages.ContainsKey(fileVersion))
+            versionStorages[fileVersion].Add(files);
+        else
+            versionStorages.Add(fileVersion, new List<WixEntity> { files });
+
+        var assemblies = Directory.GetFiles(directory, "*", SearchOption.AllDirectories);
+        Console.WriteLine($"Added '{fileVersion}' version files: ");
+        foreach (var assembly in assemblies) Console.WriteLine($"'{assembly}'");
+    }
+
+    return versionStorages.Select(storage => new Dir(storage.Key, storage.Value.ToArray())).Cast<WixEntity>().ToArray();
+}
