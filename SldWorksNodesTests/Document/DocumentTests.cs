14e372bd92ab18a02f123d498e5f4e4d82df33f1
dududu <1831197727@qq.com>
2022-03-18 10:26:42 +0800

    add test project

---
+++ SldWorksNodesTests/Document/DocumentTests.cs
@@ -0,0 +1,79 @@
+﻿using Xunit;
+using SldWorksNodes.Document;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SldWorksNodes.Document.Tests
+{
+    public class DocumentTests
+    {
+        [Fact()]
+        public void CurrentTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void OpenTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void DocumentsTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ReNameTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ConfigsTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void AsPartDocumentTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void AsAssemblyDocumentTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void AsDrawingDocumentTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ActiveConfigurationTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ConfigurationManagerTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ToStringTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
